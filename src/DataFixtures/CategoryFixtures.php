347f653483f2177f6b61ec171239585b370f1d40
Gascon-Warrior <lacaze.vincent64@gmail.com>
2022-11-16 12:24:13 +0100

    Fixtures instlled and new category added

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'Action',
+        'Aventure',
+        'Horreur',
+        'Comédie',
+        'Fantastique',
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (SELF::CATEGORIES as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+}
