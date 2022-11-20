9c9960afaccb19a759c382a9a15485cb6e4c77f8
Gascon-Warrior <lacaze.vincent64@gmail.com>
2022-11-20 20:26:42 +0100

    add template

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
 use App\Repository\ProgramRepository;
 use App\Repository\SeasonRepository;
 use Faker\Factory;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -35,6 +37,15 @@ class ProgramController extends AbstractController
     public function showSeason(Program $program, Season $season): Response
     {
         return $this->render('/program/season_show.html.twig', ['program' => $program, 'season' => $season]);
+    }
+
+    #[Route('/program/{program_id}/season/{season_id}/episode/{episode_id}', methods:['GET'], name:'program_episode_show')]
+    #[Entity('program', options: ['id' => 'program_id'])]
+    #[Entity('season', options: ['id' => 'season_id'])]
+    #[Entity('episode', options: ['id' => 'episode_id'])]
+    public function showEpisode(Program $program, Season $season, Episode $episode): Response
+    {
+        return $this->render('program/episode_show.html.twig', ['program' => $program, 'season' => $season, 'episode' => $episode]);
     }
  
 }
